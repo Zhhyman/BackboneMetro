fe999ed332be6453851a7f87276d7523dab520ba
dunauy <ivan.benito@gmail.com>
2013-06-14 14:31:37 +0200

    Obtenemos la coleccion con la lista de entidades pero sin pintarlas todavia

--- js/collections/StandCollections.js
+++ js/collections/StandCollections.js
@@ -1,3 +1,23 @@
 StandMetroVOList = Backbone.Collection.extend({
     url: '/rest/stands/vo/'
 });
+
+CarouselItemVOList = Backbone.Collection.extend({
+	// Fetching request data.
+	fairId:'2',
+	urlRoot:'/rest/carousel/list/',
+	list:[],
+	
+	url: function() {
+		return this.urlRoot + this.fairId + '.json';
+	},
+
+	parse: function(data) {
+		this.data = data;
+		var tmpList = _.first(_.toArray(data)) 
+		this.list = _(tmpList).filter( function(item) {
+			return (item.logoUrl != undefined);
+		}) ;
+	}
+});
+
--- js/views/carouselButtonView.js
+++ js/views/carouselButtonView.js
@@ -1,14 +1,47 @@
 var carouselButtonView = Backbone.View.extend({
 	el:$(this.el),
+	carouselId:'carousel',
 	
 	initialize: function() {
 		// this.model.on('change', this.render, this);
 	    // this.model.on('destroy', this.remove, this);
+		this.collection = new CarouselItemVOList();
+		this.collection.fairId = '2';
+		//this.collection.on('sync', this.render, this);
+		//this.collection.on('change', this.collectionFetchedHandler);
+		this.collection.on('error', this.collectionError, this);
+//		this.collection.fetch();
+		this.collection.fetch({
+			success: this.collectionFetchedHandler(this).handler
+		});
 	},
 	
 	render: function() {
-		var template = _.template(tpl.get('carouselButtonView'),{});
+		var template = _.template(tpl.get('carouselButtonView'),{id:this.carouselId});
 		this.$el.html(template);
+		console.log("carousel render");
 		return this;
+	},
+	
+	collectionFetchedHandler: function( target ) {
+		
+		//var collection = data;
+		var targetView = target;
+		
+		return {
+			handler: function() {
+				console.log('Collection data fetched :' + _.size(targetView.collection.list));
+				_.each(targetView.collection.list, function(item) {
+					console.log('Entity : ' + item.entityName + ' , logo : ' + item.logoUrl);
+				});
+				//targetView.render();
+			}
+			
+		}
+		
+	},
+	
+	collectionError: function() {
+		console.log('Error fetching collection');
 	}
 })
